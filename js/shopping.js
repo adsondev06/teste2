bf1ad6192d56f40ccb7634d457c76b2fad1d8f88
DevNeres <149329464+adsondev06@users.noreply.github.com>
2024-05-12 12:13:44 -0300

    teste

--- js/shopping.js
+++ js/shopping.js
@@ -1,27 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     // Verifica se existe um nome de usuário armazenado no LocalStorage
-//     const username = localStorage.getItem('username');
-    
-//     // Se existir, preenche o campo de seunome com os dados do LocalStorage
-//     if (username) {
-//         document.getElementById('seunome').value = username;
-//     }
-// });
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     $('.select2').select2();
-// });
-
-// document.getElementById('shoppingForm').addEventListener('submit', function(event) {
-//     event.preventDefault();
-    
-//     // Captura o valor selecionado no select
-//     const selectedOption = document.getElementById('shoppingSelect').value;
-    
-//     // Redireciona para a próxima página com o parâmetro na URL
-//     window.location.href = `codigos.html?shopping=${selectedOption}`;
-// });
-
 
 document.addEventListener("DOMContentLoaded", function() {
     // Verifica se existe um nome de usuário armazenado no LocalStorage
